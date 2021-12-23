6c9e355f5b66a3a616b906dcbe7739f0029cf3fc
Nicolas <nicolas.f.ollier@gmail.com>
2021-12-23 11:31:24 +0100

    mobile improvements

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ const getCat = async () => {
 
 const button = document.querySelector("#get-cat");
 button.addEventListener("click", getCat);
+
+// Resisee on mobile views
+
+function resetHeight() {
+  // reset the body height to that of the inner browser
+  document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
